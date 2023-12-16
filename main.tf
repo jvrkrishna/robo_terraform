2e0a689d91fc3806e27e9f3a6dd9ce79c6dd8b01
Rama Krishna <jvrkrishnaa@gmail.com>
2023-12-16 13:43:54 +0530

    a

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
-module "instances" {
+module "test" {
   for_each = var.component
-  source = "git::https://github.com/jvrkrishna/robo_terraform_module.git"
-  component= each.key
+  source = "Git::https://github.com/jvrkrishna/robo_terraform_module.git"
   env = var.env
+  component = each.key
 }
